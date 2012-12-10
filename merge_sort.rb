ece9fe640e0863a875fe0220c9020c38fbdc5f24
Abhishek Sharma <sharma dot abhishek dot it at-sign gmail dot com>
2012-12-10 19:45:25 +0530

    Final recursive merge sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -6,23 +6,22 @@ def recursive_merging (list, start, stop)
         list[start], list[stop] = list[stop], list[start] if list[start] > list[stop]
     else
         left_start, left_stop = start, start + (stop-start)/2
-        right_start, right_stop = start + (stop-start)/2 + 1, stop
+        right_start, right_stop = left_stop + 1, stop
         
         recursive_merging list, left_start, left_stop
         recursive_merging list, right_start, right_stop
         
         a = Array.new
-#       
-        stop-start.times do
-            break if left_start == left_stop or right_start == right_stop
+        (stop-start+1).times do
+            break if left_start > left_stop or right_start > right_stop
             expr = list[left_start] <= list[right_start]
             index = expr ? left_start : right_start
             a.push list[index]
             expr ? left_start += 1 : right_start += 1
         end
         
-        (left_start..left_stop).each { |i| a.push list[i] } unless left_start == left_stop
-        (right_start..right_stop).each { |i| a.push list[i] } unless right_start == right_stop
+        (left_start..left_stop).each { |i| a.push list[i] } unless left_start > left_stop
+        (right_start..right_stop).each { |i| a.push list[i] } unless right_start > right_stop
     
         (start..stop).each do |index|
             list[index] = a[index-start]
@@ -35,6 +34,7 @@ def merge_sort (list)
     recursive_merging list, 0, list.size - 1
 end
 
-a = [8,3,1,4,5]
+a = Array.new
+ARGV[0].split(",").map { |s| a.push s.to_i }
 merge_sort a
-puts a
+puts a.to_s
