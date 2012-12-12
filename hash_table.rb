005d63fb637ad3dd898f5fce878290faafc5e412
Abhishek Sharma <sharma dot abhishek dot it at-sign gmail dot com>
2012-12-12 09:10:18 +0530

    Hash table implementation finalized

--- hash_table.rb
+++ hash_table.rb
@@ -1,28 +1,51 @@
 class HashTable < Array
     def initialize
-        @keys = [1,2,4,6,8,9]
-        @values = [1,2,4,6,8,9]
+        @keys = []
+        @values = []
     end
+    def insert(pos, key, value)
+        @keys.insert pos, key
+        @values.insert pos, value
+    end
+
     def find(key)
         start, stop = 0, @keys.size - 1
-        until start == stop
-            middle = (stop-start)/2 + start
-            return @values[middle] if @keys[middle] == key
-            if middle == start
-                return @values[stop] if @keys[stop] == key
-                break
+        if @keys.empty? or key < @keys[start]
+            return nil, start
+        elsif key > @keys[stop]
+            return nil, @keys.size
+        else
+            until start == stop
+                middle = (stop-start)/2 + start
+                return middle if key == @keys[middle]
+                if middle == start
+                    return stop if key == @keys[stop]
+                    return nil, stop if @keys[stop]
+                end
+                start = middle if key > @keys[middle]
+                stop = middle if key < @keys[middle]
             end
-            start = middle if @keys[middle] < key
-            stop = middle if @keys[middle] > key
         end
-        return nil
     end
     def [](key)
-        return find(key)
+        pos, insert_pos = find(key)
+        return pos.nil? ? insert_pos : @values[pos]
     end
     def []=(key, value)
+        pos, insert_pos = find(key)
+        pos.nil? ? (insert insert_pos, key, value) : (@values[pos] = value)
+    end
+    def disp
+        puts @keys.to_s, @values.to_s
     end
 end
 
 a = HashTable.new
+a[1] = 1
+a[4] = 4
+a[6] = 6
+a[2] = 2
+a[9] = 9
+a[8] = 8
+a.disp
 puts a[ARGV[0].to_i]
