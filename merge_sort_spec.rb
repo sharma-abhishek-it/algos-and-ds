36b139972bd20d078846195e75a3ad75ca3fb384
Abhishek Sharma <sharma dot abhishek dot it at-sign gmail dot com>
2012-12-13 09:59:24 +0530

    Adding non recursive merge sort tests as well

--- merge_sort_spec.rb
+++ merge_sort_spec.rb
@@ -19,4 +19,22 @@ describe MergeSorter do
             end
         end
     end
+    describe "::sort_non_recursive" do
+        @tests = {
+            [] => [],
+            [1] => [1],
+            [2,1] => [1,2], [1,2] => [1,2],
+            [2,3,1] => [1,2,3], [1,2,3] => [1,2,3], [3,2,1] => [1,2,3],
+            [9,2,4,7,1,4,6,8,9,3] => [1,2,3,4,4,6,7,8,9,9]
+
+        }
+        @tests.each do |input, expected|
+            it "sorts input according to expectation" do
+                list = input
+                expected_output = expected
+                MergeSorter.sort_non_recursive list
+                list.should eql expected_output
+            end
+        end
+    end
 end
