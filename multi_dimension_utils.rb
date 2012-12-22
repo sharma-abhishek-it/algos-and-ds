b973517b59050f7de4dcb30f748b5b669473522a
Abhishek Sharma <sharma dot abhishek dot it at-sign gmail dot com>
2012-12-22 11:09:34 +0530

    finalizing matrix rotation

--- multi_dimension_utils.rb
+++ multi_dimension_utils.rb
@@ -1,16 +1,19 @@
 class Array
+    def pp
+        self.each do |i|
+            puts i.to_s
+        end
+    end
     def rotate_by_90
-        return if [0,1].include? self.size
+        return if self.size == 0 or self.size == 1
         outer_most, start = self.size - 1, 0
         (outer_most).downto(1) do |level|
             break if level - start < 1
-            puts start,level
-            a = ""
             x = start
-            start.upto(level) do |y|
-                a += [x,y].to_s
+            start.upto(level-1) do |y|
+                self[start][y],self[y][level],self[level][level-(y-start)],self[level-(y-start)][start] = 
+                    self[level-(y-start)][start],self[start][y],self[y][level],self[level][level-(y-start)]
             end
-            puts a,"*****"
             start += 1
         end
     end
